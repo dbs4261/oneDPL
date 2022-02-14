fe1942e20ad7583c31d23df759a3abfb0f667410
Sergey Kopienko <sergey.kopienko@intel.com>
2022-02-14 18:02:13 +0300

    Exclusive scan inplace test

--- test/parallel_api/numeric/numeric.ops/exclusive_scan_inplace.pass.cpp
+++ test/parallel_api/numeric/numeric.ops/exclusive_scan_inplace.pass.cpp
@@ -22,6 +22,7 @@
 #include _PSTL_TEST_HEADER(numeric)
 
 #include "support/utils.h"
+#include "support/scan_serial_impl.h"
 
 #include <CL/sycl.hpp>
 //#include <oneapi/dpl/execution>
@@ -32,10 +33,8 @@
 #endif // TEST_DPCPP_BACKEND_PRESENT
 
 #include <cassert>
-//#include <algorithm>
 #include <vector>
 #include <iostream>
-//#include <numeric>              // std::inclusive_scan, exclusive_scan
 #include <functional>
 
 #if TEST_DPCPP_BACKEND_PRESENT
@@ -45,16 +44,35 @@ test_with_usm(sycl::queue& q)
 {
     std::vector<int> v{ 3, 1, 4, 1, 5, 9, 2, 6 };
 
+    std::vector<int> serial_exclusive_scan_result(v.size());
+    exclusive_scan_serial(v.begin(), v.end(), serial_exclusive_scan_result.begin(), 0);
+
+    std::cout << "Serial exlusive scan result: ";
+    for (auto data : serial_exclusive_scan_result)
+        std::cout << data << " ";
+    std::cout << std::endl;
+
     TestUtils::usm_data_transfer<alloc_type, int> dt_helper(q, v.begin(), v.end());
     int* excl_input_dev = dt_helper.get_data();
 
     // Exclusive scan (in-place, incorrect results)
     std::exclusive_scan(v.begin(), v.end(), v.begin(), 0);
+
+    std::cout << "std::exclusive_scan result: ";
+    for (auto data : v)
+        std::cout << data << " ";
+    std::cout << std::endl;
+
     oneapi::dpl::exclusive_scan(oneapi::dpl::execution::make_device_policy(q), excl_input_dev, excl_input_dev + v.size(), excl_input_dev, 0);
 
     std::vector<int> excl_result_host_data_vector(v.size(), 0);
     int* excl_result_host = excl_result_host_data_vector.data();
     q.memcpy(excl_result_host, excl_input_dev, v.size() * sizeof(int)).wait();
+
+    std::cout << "oneapi::dpl::exclusive_scan result: ";
+    for (auto data : excl_result_host_data_vector)
+        std::cout << data << " ";
+    std::cout << std::endl;
 
     for (int i = 0; i < v.size(); i++)
     {
