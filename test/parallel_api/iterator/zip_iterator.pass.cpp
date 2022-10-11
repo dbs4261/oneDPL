e6032b436e8524f93bd84a852e4f768fa36df3d7
Sergey Kopienko <sergey.kopienko@intel.com>
2022-10-11 16:15:03 +0200

    std::get -> dpl::get in tests

--- test/parallel_api/iterator/zip_iterator.pass.cpp
+++ test/parallel_api/iterator/zip_iterator.pass.cpp
@@ -86,7 +86,7 @@ struct TupleNoOp
     }
 };
 
-using ::std::get;
+using dpl::get;
 template <typename Predicate, int KeyIndex>
 struct TuplePredicate
 {
@@ -709,7 +709,7 @@ DEFINE_TEST(test_lexicographical_compare)
 struct Assigner{
     template<typename T>
     bool operator()(T x) const {
-        using ::std::get;
+        using dpl::get;
         return get<1>(x) != 0;
     }
 };
