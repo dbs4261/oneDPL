69e2b59dc7de0bf3e8ffbeba80f427b511a50e65
Julian Miller <julian.miller@intel.com>
2022-12-07 18:49:08 +0100

    Improve order of UDTKind
    Signed-off-by: Julian Miller <julian.miller@intel.com>

--- test/support/utils_sycl.h
+++ test/support/utils_sycl.h
@@ -319,8 +319,8 @@ test4buffers(int mult = kDefaultMultValue)
         // 2. create pointers at first+offset
         auto inout1_offset_first = test_base_data.get_start_from(UDTKind::eKeys);
         auto inout2_offset_first = test_base_data.get_start_from(UDTKind::eVals);
-        auto inout3_offset_first = test_base_data.get_start_from(UDTKind::eResKeys);
-        auto inout4_offset_first = test_base_data.get_start_from(UDTKind::eRes);
+        auto inout3_offset_first = test_base_data.get_start_from(UDTKind::eRes);
+        auto inout4_offset_first = test_base_data.get_start_from(UDTKind::eRes2);
 
         // 3. run algorithms
         for (size_t n = 1; n <= max_n; n = (n <= 16 ? n + 1 : size_t(3.1415 * n)))
@@ -348,8 +348,8 @@ test4buffers(int mult = kDefaultMultValue)
         // 2. create iterators over buffers
         auto inout1_offset_first = test_base_data.get_start_from(UDTKind::eKeys);
         auto inout2_offset_first = test_base_data.get_start_from(UDTKind::eVals);
-        auto inout3_offset_first = test_base_data.get_start_from(UDTKind::eResKeys);
-        auto inout4_offset_first = test_base_data.get_start_from(UDTKind::eRes);
+        auto inout3_offset_first = test_base_data.get_start_from(UDTKind::eRes);
+        auto inout4_offset_first = test_base_data.get_start_from(UDTKind::eRes2);
 
         // 3. run algorithms
         for (size_t n = 1; n <= max_n; n = (n <= 16 ? n + 1 : size_t(3.1415 * n)))
